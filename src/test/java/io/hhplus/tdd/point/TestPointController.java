7cffab9685ed2bc13f685fa927a6c805e6afded0
안승현 <abeb3@naver.com>
2025-10-20 21:38:45 +0900

    getUserPoint 실패케이스 작성 실제 Controller 호출

--- src/test/java/io/hhplus/tdd/point/TestPointController.java
+++ src/test/java/io/hhplus/tdd/point/TestPointController.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,12 +15,16 @@ class TestPointController {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@MockBean
+	private PointController pointController;
+
 	@Test
 	@DisplayName("사용자의 포인트 조회")
 	void getUserPoint() throws Exception {
-		UserPoint userPoint = new UserPoint(1L, 0, System.currentTimeMillis());
+		UserPoint testUserPoint = new UserPoint(1L, 100, System.currentTimeMillis());
+		UserPoint userPoint = pointController.point(1);
 
-		assertEquals(new UserPoint(2L, 100, System.currentTimeMillis()), userPoint);
+		assertEquals(testUserPoint, userPoint);
 	}
 
 }
